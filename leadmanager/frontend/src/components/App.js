7eae1b9acf7e011273edbc643293433bb894325d
daekunhan <dosemfb0920@gmail.com>
2021-05-30 17:16:27 +0900

    alert added

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -1,17 +1,37 @@
 import React, { Component, Fragment } from 'react';
 import ReactDOM from 'react-dom';
+
+import { Provider as AlertProvider } from 'react-alert';
+import AlertTemplate from 'react-alert-template-basic';
+import Positions from 'react-alert-template-basic';
+
 import Header from './layout/Header';
 import DashBoard from './leads/DashBoard';
+import Alert from './layout/Alert';
+
+import { Provider } from 'react-redux';
+import store from '../store';
+
+const alertOptions = {
+    timeout: 3000,
+    position: Positions.TOP_CENTER
+}
+
+
 
 class App extends React.Component {
     render() {
         return (
-            <Fragment>
-                <Header />
-                <div className="container">
-                    <DashBoard />    
-                </div>
-            </Fragment>
+            <Provider store={store}>
+                <AlertProvider template={AlertTemplate} 
+                {...alertOptions}>
+                    <Header />
+                    <Alert />
+                    <div className="container">
+                        <DashBoard />    
+                    </div>
+                </AlertProvider>
+            </Provider>
         );
     }
 }
